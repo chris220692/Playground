bd43ae0320064fc5f6a3e5170a7901e32e84551d
Julian Stas <julian.stas@gmail.com>
2018-11-19 15:17:10 +0100

    added sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,39 @@ import "bootstrap";
 import $ from "jquery";
 import { fullcalendar } from "fullcalendar";
 
+function sideBar() {
+  console.log('AM I REAL')
+  $(".sidebar-dropdown > a").click(function() {
+    $(".sidebar-submenu").slideUp(200);
+      if (
+        $(this)
+          .parent()
+          .hasClass("active")
+      ) {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+          .parent()
+          .removeClass("active");
+      } else {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+          .next(".sidebar-submenu")
+          .slideDown(200);
+        $(this)
+          .parent()
+          .addClass("active");
+        }
+      });
+  $("#close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
+}
+
+sideBar();
+
 $(function () {
 $('#calendar').fullCalendar({
   events: '/calendar_events',
