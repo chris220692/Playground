dea088bd0c7c15732018503e469b4efb1b4ff632
Julian Stas <julian.stas@gmail.com>
2018-11-20 12:15:24 +0100

    massegas displays email nom instead of user id

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,17 @@ import "bootstrap";
 import $ from "jquery";
 import { fullcalendar } from "fullcalendar";
 
+$(function () {
+$('#calendar').fullCalendar({
+  events: '/calendar_events',
+  header:{
+    left: "month,agendaWeek, agendaDay",
+    center: "title",
+    right: "today, prevYear,prev,next,nextYear"
+  }
+});
+})
+
 function sideBar() {
   console.log('AM I REAL')
   $(".sidebar-dropdown > a").click(function() {
@@ -34,31 +45,3 @@ function sideBar() {
 }
 
 sideBar();
-
-
-
-
-function toggle_div_hide(id) {
-var divelement = document.getElementById(id);
-  console.log(divelement)
-  if(divelement.style.display == 'none') {
-    divelement.style.display = 'block';
-  }
-  else {
-    divelement.style.display = 'none'
-  }
-}
-document.getElementById('hide-calendar').addEventListener('click', function() { toggle_div_hide('calendar') })
-
-
-
-$(function () {
-$('#calendar').fullCalendar({
-  events: '/calendar_events',
-  header:{
-    left: "month,agendaWeek, agendaDay",
-    center: "title",
-    right: "today, prevYear,prev,next,nextYear"
-  }
-});
-})
